a3ce5b35d0ad94f0e17b646613ea2e5576e724ed
Stefan <stefanbentz@gmx.de>
2025-02-28 12:42:37 +0100

    Mehr Befehle

--- 02_SQL_Tables/02a_db_table_SQL.sql
+++ 02_SQL_Tables/02a_db_table_SQL.sql
@@ -1,30 +1,38 @@
-/* ------  Strukturen ----- */
+-- /* ------  Strukturen ----- */
+\! cls
+-- /* Kommentar 1 */
+-- #  Kommentar 2
+-- -- Kommentar 3
 
-/* Kommentar 1 */
-#  Kommentar 2
--- Kommentar 3
+-- /* Datenbanken auf Server anzeigen */
+SHOW DATABASES;
 
-/* Datenbanken auf Server anzeigen */
+-- /* DB boo löschen, falls vorhanden*/
+DROP DATABASE IF EXISTS boo;
 
+-- /* DB boo anlegen, falls noch nicht vorhanden*/
+CREATE DATABASE IF NOT EXISTS boo;
 
-/* DB boo löschen, falls vorhanden*/
+-- /* DB auswählen */
+USE boo;
+-- /* Tabelle anlegen */
+CREATE TABLE IF NOT EXISTS test
+(
+    name VARCHAR(20),
+    age INT
 
+);
 
-/* DB boo anlegen, falls noch nicht vorhanden*/
+-- /* Alle Tabellen in der DB anzeigen */
+SHOW TABLES;
 
+-- /* Struktur der Tabelle anzeigen */
+DESCRIBE test;
 
-/* DB auswählen */
+-- /* ----- Daten ------- */
+INSERT INTO test(name, age) VALUES("Stefan", 43);
+INSERT INTO test (age, name) VALUES(39, "Andreas");
+INSERT INTO test VALUES();
 
-/* Tabelle anlegen */
-
-
-/* Alle Tabellen in der DB anzeigen */
-
-
-/* Struktur der Tabelle anzeigen */
-
-
-/* ----- Daten ------- */
-
-
-/* ---- Inhalte der Tabelle anzeigen ---- */
+-- /* ---- Inhalte der Tabelle anzeigen ---- */
+SELECT * FROM test;
